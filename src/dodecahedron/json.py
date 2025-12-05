4f51e37e45bd770b199a6461ee79be4f1e17d2eb
Sean Sullivan <43550769+seanssullivan@users.noreply.github.com>
2025-12-05 15:38:33 -0700

    Add custom JSON encoder class.

--- src/dodecahedron/json.py
+++ src/dodecahedron/json.py
@@ -3,20 +3,20 @@
 # Standard Library Imports
 import datetime
 import json
-import logging
 import pathlib
 from typing import Any
+from typing import Dict
+from typing import Hashable
+from typing import Optional
 
-# Third-Party Imports
-# TODO: Make numpy and pandas optional.
-import numpy as np
-import pandas as pd
+# Local Imports
+from .helpers import import_module
+
+# Optional Third-Party Imports
+np = import_module("numpy", required=False)
+pd = import_module("pandas", required=False)
 
 __all__ = ["JSONEncoder"]
-
-
-# Initiate logger.
-log = logging.getLogger(__name__)
 
 
 class JSONEncoder(json.JSONEncoder):
@@ -24,17 +24,10 @@ class JSONEncoder(json.JSONEncoder):
 
     def encode(self, o: Any, *args: Any, **kwargs: Any) -> str:
         """Return a JSON string representation of a Python data structure."""
-        return super().encode(remove_nan(o), *args, **kwargs)
+        result = super().encode(replace_nan(o), *args, **kwargs)
+        return result
 
     def default(self, o: object) -> Any:
-        # TODO: Only check if pandas is installed.
-        if pd.isnull(o):
-            return None
-
-        # TODO: Only check if numpy is installed.
-        if isinstance(o, np.bool_):
-            return bool(o)
-
         if isinstance(o, datetime.datetime):
             return str(o)
 
@@ -44,18 +37,39 @@ class JSONEncoder(json.JSONEncoder):
         if isinstance(o, pathlib.Path):
             return str(o.resolve())
 
+        # Only check when numpy is installed.
+        if np is not None and isinstance(o, getattr(np, "bool_")):
+            return bool(o)
+
+        # Only check when pandas is installed.
+        if pd is not None and getattr(pd, "isnull")(o):
+            return None
+
         return super().default(o)
 
 
-def remove_nan(obj: Any) -> Any:
-    """Remove NaN."""
-    if isinstance(obj, dict):
-        return {key: remove_nan(value) for key, value in obj.items()}
+def replace_nan(__value: Any, repl: Optional[str] = None) -> Any:
+    """Replace 'np.nan'.
 
-    if isinstance(obj, list):
-        return [remove_nan(item) for item in obj]
+    Args:
+        __value: Value in which to replace 'np.nan'.
+        repl: Value with which to replace 'np.nan'.
 
-    if pd.isnull(obj):
-        return None
+    Returns:
+        Value.
 
-    return obj
+    """
+    if isinstance(__value, dict):
+        result: Dict[Hashable, Any] = {
+            k: replace_nan(v) for k, v in __value.items()  # type: ignore
+        }
+        return result
+
+    if isinstance(__value, list):
+        result = [replace_nan(i) for i in __value]  # type: ignore
+        return result
+
+    if pd and getattr(pd, "isnull")(__value):
+        return repl
+
+    return __value
