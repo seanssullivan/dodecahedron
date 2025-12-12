d8cc11e915cc89a05fdbb76b9e70a4241fab303c
Sean Sullivan <43550769+seanssullivan@users.noreply.github.com>
2025-12-11 22:04:19 -0700

    Add tests for dictionary mapper.

--- tests/unit/mappers/test_dictionary_mapper.py
+++ tests/unit/mappers/test_dictionary_mapper.py
@@ -34,6 +34,50 @@ def test_instantiates_dictionary_from_dictionary(
 
 @pytest.mark.parametrize(
     "schema,properties",
+    [({"value": {"map_to": "name", "converters": {"inward": int}}}, {})],
+)
+def test_converts_value_when_instantiating_from_dictionary(
+    schema: Dict[Hashable, Any],
+    properties: Dict[Hashable, Any],
+) -> None:
+    mapper = DictMapper(schema, properties)
+    result = mapper.from_dict({"name": "1"})
+    assert isinstance(result["value"], int)
+
+
+@pytest.mark.parametrize(
+    "schema,properties",
+    [
+        ({}, {"value": 0}),
+        ({0: "value"}, {}),
+        ({0: {"map_to": "value"}}, {}),
+    ],
+)
+def test_instantiates_class_from_list(
+    schema: Dict[Hashable, Any],
+    properties: Dict[Hashable, Any],
+) -> None:
+    mapper = DictMapper(schema, properties)
+    result = mapper.from_list(["success"])
+    assert isinstance(result, dict)
+    assert result["value"] == "success"
+
+
+@pytest.mark.parametrize(
+    "schema,properties",
+    [({0: {"map_to": "value", "converters": {"inward": str}}}, {})],
+)
+def test_converts_value_when_instantiating_from_list(
+    schema: Dict[Hashable, Any],
+    properties: Dict[Hashable, Any],
+) -> None:
+    mapper = DictMapper(schema, properties)
+    result = mapper.from_list([1])
+    assert isinstance(result["value"], str)
+
+
+@pytest.mark.parametrize(
+    "schema,properties",
     [
         ({}, {"name": "value"}),
         ({"value": "name"}, {}),
@@ -52,19 +96,6 @@ def test_converts_dictionary_to_dictionary(
 
 @pytest.mark.parametrize(
     "schema,properties",
-    [({"value": {"map_to": "name", "converters": {"inward": int}}}, {})],
-)
-def test_converts_value_when_instantiating_from_dictionary(
-    schema: Dict[Hashable, Any],
-    properties: Dict[Hashable, Any],
-) -> None:
-    mapper = DictMapper(schema, properties)
-    result = mapper.from_dict({"name": "1"})
-    assert isinstance(result["value"], int)
-
-
-@pytest.mark.parametrize(
-    "schema,properties",
     [({"value": {"map_to": "name", "converter": str}}, {})],
 )
 def test_converts_value_when_converting_to_dictionary(
@@ -74,3 +105,34 @@ def test_converts_value_when_converting_to_dictionary(
     mapper = DictMapper(schema, properties)
     result = mapper.to_dict({"value": 1})
     assert isinstance(result["name"], str)
+
+
+@pytest.mark.parametrize(
+    "schema,properties",
+    [
+        ({}, {"value": 0}),
+        ({0: "value"}, {}),
+        ({0: {"map_to": "value"}}, {}),
+    ],
+)
+def test_converts_class_to_list(
+    schema: Dict[Hashable, Any],
+    properties: Dict[Hashable, Any],
+) -> None:
+    mapper = DictMapper(schema, properties)
+    result = mapper.to_list({"value": "success"})
+    assert isinstance(result, list)
+    assert result[0] == "success"
+
+
+@pytest.mark.parametrize(
+    "schema,properties",
+    [({0: {"map_to": "value", "converter": int}}, {})],
+)
+def test_converts_value_when_converting_to_list(
+    schema: Dict[Hashable, Any],
+    properties: Dict[Hashable, Any],
+) -> None:
+    mapper = DictMapper(schema, properties)
+    result = mapper.to_list({"value": "1"})
+    assert isinstance(result[0], int)
